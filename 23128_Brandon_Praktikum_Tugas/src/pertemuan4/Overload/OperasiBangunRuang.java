15fe27ac1dd355e16db210312af9a8a87511215f
Brandon <95530290+BrandonLastomo@users.noreply.github.com>
2024-09-24 23:40:18 +0700

    add additional comment

--- 23128_Brandon_Praktikum_Tugas/src/pertemuan4/Overload/OperasiBangunRuang.java
+++ 23128_Brandon_Praktikum_Tugas/src/pertemuan4/Overload/OperasiBangunRuang.java
@@ -14,7 +14,9 @@ public class OperasiBangunRuang {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // membuat objek dari class RumusBangunRuang
         RumusBangunRuang rumus = new RumusBangunRuang();
+        // inisiasi ukuran bangun ruang
         double s = 2.5;
         double p = 3.75;
         double l = 8.52;
@@ -22,17 +24,17 @@ public class OperasiBangunRuang {
         
         // kubus
         System.out.println("=====Luas Permukaan dan Volume Kubus=====");
-        // luas permukaan
+        // menampilkan luas permukaan kubus
         System.out.println("Luas permukaan dari kubus dengan sisi " + s + "cm adalah: " + rumus.luas(s) + " cm");
-        // volume
+        // menampilkan volume kubus
         System.out.println("Volume dari kubus dengan sisi " + s + "cm adalah: " + rumus.volume(s) + " cm");
         System.out.println("==============================");
         
         // balok
         System.out.println("\n=====Luas Permukaan dan Volume Balok=====");
-        // luas permukaan
+        // menampilkan luas permukaan balok
         System.out.println("Luas permukaan dari balok dengan panjang " + p + " cm, lebar " + l + "cm, tinggi " + t + " cm " + "adalah: " + rumus.luas(p, l, t) + " cm");
-        // volume
+        // menampilkan volume balok
         System.out.println("Volume dari balok dengan panjang " + p + " cm, lebar " + l + "cm, tinggi " + t + " cm " + "adalah: " + rumus.volume(p, l, t) + " cm");
         System.out.println("==============================");
     }
